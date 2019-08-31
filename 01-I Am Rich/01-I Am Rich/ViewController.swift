1a3a9e0110d1b4cb838393e1f48ae67d84869d79
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2019-08-31 12:31:28 +0200

    Init project 01 - I am rich

---
+++ 01-I Am Rich/01-I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  01-I Am Rich
+//
+//  Created by Sergio Sánchez Sánchez on 31/08/2019.
+//  Copyright © 2019 Sergio Sánchez Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func onStartClicked(_ sender: UIButton) {
+        
+        print("Start Button Clicked")
+        
+    }
+}
+
