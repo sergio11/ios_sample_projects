3bb0fb260eb81a64263fa0441f63a2aa8449ce69
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2019-08-31 13:23:48 +0200

    Add Info Dialog

--- 01-I Am Rich/01-I Am Rich/ViewController.swift
+++ 01-I Am Rich/01-I Am Rich/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var infoImage: UIImageView! {
+        didSet {
+            infoImage.isUserInteractionEnabled = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let infoImageGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(onInfoImageClicked))
+        infoImage.addGestureRecognizer(infoImageGestureRecognizer)
+    
     }
-
+    
+    //Action
+    @objc func onInfoImageClicked() {
+        
+        let alertController = UIAlertController(title: "I Am Rich", message: "Diálogo con información sobre el objetivo del juego", preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+        
+        show(alertController, sender: self)
+        
+    }
+    
 
     @IBAction func onStartClicked(_ sender: UIButton) {
         
