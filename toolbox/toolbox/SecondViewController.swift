8f856ef015f6e28e162c60c7ebde9952062a4043
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2019-09-09 22:27:59 +0200

    Project ToolBox

--- toolbox/toolbox/SecondViewController.swift
+++ toolbox/toolbox/SecondViewController.swift
@@ -8,11 +8,53 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var fontFamilyTableView: UITableView!
+    
+    var families : [String]  = []
+    var fonts : [String: [String]] = [:]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        families = UIFont.familyNames.sorted(by: { return $0 < $1 })
+        
+        for fam in families{
+            fonts[fam] = UIFont.fontNames(forFamilyName: fam)
+        }
+        
+        print(fonts)
+        
+        self.fontFamilyTableView.delegate = self
+        self.fontFamilyTableView.dataSource = self
+    }
+    
+    // MARK: - Métodos del protocolo UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.families.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FontFamilyTableCell", for: indexPath)
+        let fontFamily = families[indexPath.row]
+        cell.textLabel?.text = fontFamily
+        cell.textLabel?.font = UIFont(name: fontFamily, size: 20.0)
+        return cell
+    }
+    
+    
+    //MARK: - Métodos del protocolo UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let row = indexPath.row
+        let fontFamily = families[row]
+        let familyFonts = fonts[fontFamily]!
+        print("didSelectRowAt -> \(familyFonts), familyFonts -> \(familyFonts)")
     }
 
 
