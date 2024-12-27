868b7ffb1bdf9a35351ea3cbb9fe4626712a5d78
Sergio Sánchez <sss4esob@gmail.com>
2024-12-27 12:47:40 +0100

    Preparing initial assets (app icon set, sounds, main logo)

---
+++ Honeymoon/Honeymoon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Honeymoon
+//
+//  Created by Sergio Sánchez Sánchez on 27/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Honeymoon/Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Sergio Sánchez Sánchez on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
