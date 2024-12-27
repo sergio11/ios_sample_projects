9e359c591b8cc8e0d0a4dae42e97e3013ec65bac
Sergio Sánchez <sss4esob@gmail.com>
2024-12-27 14:30:20 +0100

    Implement binding

--- Honeymoon/Honeymoon/View/ContentView.swift
+++ Honeymoon/Honeymoon/View/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: - PROPERTIES
+    @State var showAlert: Bool = false
+    
     var body: some View {
         VStack {
             HeaderView()
@@ -15,9 +18,15 @@ struct ContentView: View {
             CardView(honeymoon: honeymoonData[2])
                 .padding()
             Spacer()
-            FooterView()
+            FooterView(showBookingAlert: $showAlert)
         }
-        
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("SUCCESS"),
+                message: Text("Whishing a lovely and most precious of the times together for the amazing couple."),
+                dismissButton: .default(Text("Happy Honeymoon!"))
+            )
+        }
     }
 }
 
--- Honeymoon/Honeymoon/View/FooterView.swift
+++ Honeymoon/Honeymoon/View/FooterView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct FooterView: View {
+    
+    // MARK: PROPERTIES
+    @Binding var showBookingAlert: Bool
+    
+    
     var body: some View {
         HStack {
             Image(systemName: "xmark.circle")
@@ -15,7 +20,7 @@ struct FooterView: View {
             Spacer()
             
             Button(action: {
-                print("Success!")
+                self.showBookingAlert.toggle()
             }) {
                 Text("Book Destination".uppercased())
                     .font(.system(.subheadline, design: .rounded))
@@ -38,8 +43,11 @@ struct FooterView: View {
 }
 
 struct FooterView_Previews: PreviewProvider {
+    
+    @State static var showAlert: Bool = false
+    
     static var previews: some View {
-        FooterView()
+        FooterView(showBookingAlert: $showAlert)
             .previewLayout(.fixed(width: 375, height: 80))
     }
 }
